f6d5fac9cee757196ded4d53a68bb8f588213cec
woodbrittany <woodbrittany3@gmail.com>
2022-01-15 16:27:17 -0600

    Color Scheme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import "./App.css";
 
-function App() {
+export default function App() {
+  const colors = [
+    "#9253a1",
+    "#f063a4",
+    "#2dc5f4",
+    "#fcee21",
+    "#f16164",
+    "#70327e",
+    "#a42963",
+    "#0b6a88",
+    "#f89e4f",
+    "#ec015a"
+  ];
+
+  const [background, setBackground] = useState("#071415");
+  const [current, setCurrent] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" style={{ background: background }}>
+      {current !== null && <h1>Copied {current}</h1>}
+    <div className="container">
+      {colors.map((color, index) => (
+        <div key={index} className="card">
+          <div style={{ 
+            background: color,
+            filter: "brightness(85%)",
+           boxShadow: color === background ? "0 0 5px #000" : ""
+           }}
+          className="box" 
+          onClick={() => setBackground(color)}
+          />
+          <CopyToClipboard text={`color: ${color};`}>
+            <p 
+              style={{ color: color === background ? "#fff" : color }} 
+              onClick={() => setCurrent(color)}>
+              {color}
+          </p>
+          </CopyToClipboard>
+          </div>
+         ))}
+      </div>
+   </div>
   );
-}
-
-export default App;
+  }
